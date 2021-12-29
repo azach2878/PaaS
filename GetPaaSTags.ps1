0fb3a1655967de3db21253ff75d7cee8cbc52957
Adam <51384650+azach2878@users.noreply.github.com>
2021-12-29 14:26:04 -0700

    Create GetPaaSTags.ps1

---
+++ GetPaaSTags.ps1
@@ -0,0 +1,412 @@
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param
+    (
+    [Parameter(Mandatory=$true)] [String]$SubscriptionID
+)
+
+Connect-AzAccount -WarningAction SilentlyContinue 
+Get-AzSubscription -SubscriptionId $SubscriptionID | Select-AzSubscription 
+$SubscriptionName = (Get-AzSubscription -SubscriptionId $SubscriptionID).Name
+
+Write-Host "You Are Connected to Azure $SubscriptionName" -BackgroundColor Black -ForegroundColor Cyan 
+
+$AZResources = Get-AzResource | select *
+        $results      = @()
+        $tagsString   = ""
+
+foreach ($resource in $AZResources) {
+
+    # 1
+    if ($resource.Kind -eq "functionapp" -and $resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Web/sites") {
+        
+        $ResourceType    = "Function App"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    # 2
+    if ($resource.Kind -eq "functionapp" -and $resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Web/sites") {
+        
+        $ResourceType    = "Function App"
+        $Tags            = $Resource.Tags
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+
+    #3
+    if ($resource.Kind -eq "app" -and $resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Web/sites") {
+        
+        $ResourceType    = "App Service"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+
+    #4
+    if ($resource.Kind -eq "app" -and $resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Web/sites") {
+        
+        $ResourceType    = "App Service"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+  
+    #5
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.DataFactory/factories") {
+        
+        $ResourceType    = "Data Factory"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #6
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.DataFactory/factories") {
+        
+        $ResourceType    = "Data Factory"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+
+    #7
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Logic/workflows") {
+        
+        $ResourceType    = "Logic App"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #8
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Logic/workflows") {
+        
+        $ResourceType    = "Logic App"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }   
+
+
+
+    #9
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Databricks/workspaces") {
+        
+        $ResourceType    = "Data Bricks"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #10
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Databricks/workspaces") {
+        
+        $ResourceType    = "Data Bricks"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }   
+
+
+    #11
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Synapse/workspaces") {
+        
+        $ResourceType    = "Azure Synapse"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #12
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Synapse/workspaces") {
+        
+        $ResourceType    = "Azure Synapse"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }   
+
+
+    #13
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Sql/servers/databases") {
+        
+        $ResourceType    = "Azure SQL Database"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #14
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Sql/servers/databases") {
+        
+        $ResourceType    = "Azure SQL Database"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+}   
+
+    #15
+    if ($resource.Tags.Count -ige "1" -and $resource.ResourceType -eq "Microsoft.Sql/servers") {
+        
+        $ResourceType    = "Azure SQL Server"
+        $Tags            = $Resource.Tags
+        $Tags.GetEnumerator() | ForEach-Object {
+
+        $TagValueString  = $_.Value + "]" 
+        $TagNameString   = "[" + $_.Key 
+        $tagsString += "$TagNameString = $TagValueString "
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+}
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+  }
+    
+    
+    #16
+    if ($resource.Tags.Count -eq "0" -and $resource.ResourceType -eq "Microsoft.Sql/servers") {
+        
+        $ResourceType    = "Azure SQL Server"
+        $tagsString      = "Null"
+
+        $details = @{ 
+
+                Tags = $TagsString
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+  }                           
+
+    $results += New-Object PSObject -Property $details 
+    $tagsString = ""
+}   
+
+
+# End
+} 
+
+$results | Export-Csv .\apptest.csv -NoTypeInformation -Force
+.\apptest.csv
